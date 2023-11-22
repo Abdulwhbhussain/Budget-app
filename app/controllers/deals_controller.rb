4fe3eaf618f57d2a41449bdf90e12cadbd24a0bc
WHB901 <abdulwhbhussain@gmail.com>
2023-11-22 08:59:06 +0500

    Raw minimal working views

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,16 +1,18 @@
 class DealsController < ApplicationController
-
   def new
-    @user = current_user
-    @deal = @user.deals.new
+    @deal = Deal.new
+    @categories = Category.where(author: current_user)
   end
 
   def create
-    @user = current_user
-    @deal = @user.deals.new(deal_params)
+    @deal = Deal.new(deal_params)
+    @deal.author = current_user
     
+    # it contains a many to many relationship and joins table between deals and categories
+    @deal.categories = Category.where(id: params[:category])
+
     if @deal.save
-      redirect_to category_path(@category)
+      redirect_to category_path(Category.find_by(id: params[:category]))
     else
       render :new
     end
